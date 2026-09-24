44c19c530199632182014a0926c19cdd0b96b8da
JB-Tech95-admin <andriamirado.heritiana301@gmail.com>
2026-09-24 09:50:26 +0200

    feat/ creation du projet

---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.tsx'
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+
+import App from "./App"
+import "./assets/index.css"
+
+import { TooltipProvider } from "@/components/ui/tooltip"
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <TooltipProvider>
+      <App />
+    </TooltipProvider>
+  </StrictMode>,
+)
